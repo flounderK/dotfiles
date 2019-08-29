1e9a7f56b2a4671423c5c8a4879c4d7d012ae532
Clif Wolfe <clifwolfe1@gmail.com>
2019-08-29 13:35:47 -0400

    add powerline fonts

--- vim_deps.sh
+++ vim_deps.sh
@@ -3,4 +3,12 @@
 echo "Installing Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+version=$(uname -v)
+debbased=$(echo $version | grep -Pio "(debian|ubuntu)")
+if [[ ! -z "$debbased" ]]
+then
+	apt update 
+	apt install fonts-powerline
+fi
+
 
