cd5a2d175b1bf49dd73f7c72e9dcd83a9ecaffe5
Clif Wolfe <clifwolfe1@gmail.com>
2020-04-26 13:44:05 -0400

    fix zsh setup

--- dotfilesInstallScripts/zshsetup.sh
+++ dotfilesInstallScripts/zshsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh -s -- --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
